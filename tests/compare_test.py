24e29daec1395c0208a1f85f61a83fd1580adc94
Jonny Reading <jonnyreading@gmail.com>
2020-07-22 17:05:15 +0100

    New test for different numbers, compare function still just returns string so second test fails

--- tests/compare_test.py
+++ tests/compare_test.py
@@ -6,3 +6,6 @@ class TestCompare(unittest.TestCase):
 
     def test_compare_3_1_returns_3_is_greater_than_1(self):
         self.assertEqual("3 is greater than 1", compare(3, 1))
+
+    def test_compare_6_3_returns_6_is_greater_than_3(self):
+        self.assertEqual("6 is greater than 3", compare(6, 3))
