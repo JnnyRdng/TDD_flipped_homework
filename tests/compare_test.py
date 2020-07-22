2a4459fd2e46eae696f766fdb4fe369aed1e6036
Jonny Reading <jonnyreading@gmail.com>
2020-07-22 17:08:34 +0100

    New test wants a different returned string if the first number is less than the second
    Will require logic to choose which string is returned else the first two tests will fail

--- tests/compare_test.py
+++ tests/compare_test.py
@@ -9,3 +9,6 @@ class TestCompare(unittest.TestCase):
 
     def test_compare_6_3_returns_6_is_greater_than_3(self):
         self.assertEqual("6 is greater than 3", compare(6, 3))
+
+    def test_compare_2_8_returns_2_is_less_than_8(self):
+        self.assertEqual("2 is less than 8", compare(2, 8))
